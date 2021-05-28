56c8feb4afbb00e4d7924557833e0a4bf6bb89e3
JWShuff <16825267+JWShuff@users.noreply.github.com>
2021-05-28 16:31:57 -0400

    JWS/Python solution and unit tests

--- python/calculate_mode.py
+++ python/calculate_mode.py
@@ -1,2 +1,34 @@
-def calculate_mode():
-    pass
+def calculate_mode(input_list):
+    # -Define a function which is called with INPUT_LIST
+    # -Define a variable COUNTER
+    # -Create a list ANSWER_LIST which will hold return values
+    # -Create a list CANDIDATE_LIST which will receive all values
+    # and count of occurrences. 
+    # -Iterate through INPUT_LIST counting instances of unique values
+    # -append the value and count to CANDIDATE_LIST
+    # -Iterate through finding highest count and appending to ANSWER_LIST
+    # - If multiple entries with same count, append all tie values to ANSWER_LIST
+
+
+    answer_list = []
+    candidate_list = []
+    highest_value = 0
+
+    for x in range(0, len(input_list)):
+        occurrences = 0
+        occurrences = input_list.count(input_list[x])
+        push_list = []
+        push_list.append(input_list[x])
+        push_list.append(occurrences)
+        candidate_list.append(push_list)
+        if candidate_list[x][1] > highest_value:
+            highest_value = candidate_list[x][1]
+
+    # Add values that occurred the most number of times
+    for x in range(0, len(candidate_list)):
+        if candidate_list[x][1] == highest_value:
+            if candidate_list[x][0] not in answer_list:
+                answer_list.append(candidate_list[x][0])
+    return answer_list
+
+#print(calculate_mode([1,2,3,3]))
--- python/calculate_mode_spec.py
+++ python/calculate_mode_spec.py
@@ -1,1 +1,33 @@
-# Write your own unit tests here!
+import unittest
+from calculate_mode import calculate_mode
+
+
+class TestCalculateMode(unittest.TestCase):
+
+    """
+    When you call calculate_mode you get a list back
+    """
+
+    def test_returns_a_list(self):
+        self.assertTrue(type(calculate_mode([1, 2])) is list)
+    """
+    Test when called with lists returns the proper mode:
+    calculate_mode([1,2,3,3]) => [3]
+    calculate_mode([4.5, 0, 0]) => [0]
+    calculate_mode([1.5, -1, 1, 1.5]) => [1.5]
+    calculate_mode([1,1,2,2]) => [1,2]
+    calculate_mode([1,2,3]) => [1,2,3]
+    because all occur with equal frequency
+    calculate_mode(["who", "what", "where", "who"]) => ["who"]
+    """
+
+    def test_outputs(self):
+        self.assertTrue(calculate_mode([1, 2, 3, 3]) == [3])
+        self.assertTrue(calculate_mode([4.5, 0, 0]) == [0])
+        self.assertTrue(calculate_mode([1.5, -1, 1, 1.5]) == [1.5])
+        self.assertTrue(calculate_mode([1, 1, 2, 2]) == [1,2])
+        self.assertTrue(calculate_mode([1, 2, 3]) == [1, 2, 3])
+        self.assertTrue(calculate_mode(["who", "what", "where", "who"]) == ["who"])
+
+if __name__ == '__main__':
+    unittest.main()
