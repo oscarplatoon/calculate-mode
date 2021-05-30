0c1413b8e8ee73958097fe72434ce9a65850980b
paulikas78 <70077913+paulikas78@users.noreply.github.com>
2021-05-29 20:17:59 -0500

    Jonas' calculate-mode challenge

--- js/calculateMode.js
+++ js/calculateMode.js
@@ -1,3 +1,22 @@
-const calculateMode = () => {
+const calculateMode = (arr) => {
+let modeObj = {};
+  let count = 0;
+  let modeArr = [];
 
+  for (let i = 0; i < arr.length; i++) {
+    let char = arr[i];
+
+    modeObj[char] = (modeObj[char] || 0) + 1
+
+    if (modeObj[char] > count) {
+      count = modeObj[char];
+    }
+  }
+    for (highNum in modeObj) {
+      if (modeObj[highNum] === count) {
+      modeArr.push(highNum) //Figure out how to add int/float/string as needed
+    } 
+  }
+
+  return modeArr;
 }
