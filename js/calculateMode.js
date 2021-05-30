77f604098f139d796c5348bb44ad92af42f0a4f2
jung341416 <jung341416@gmail.com>
2021-05-30 04:13:54 -0400

    pseudo code

--- js/calculateMode.js
+++ js/calculateMode.js
@@ -1,4 +1,13 @@
 const calculateMode = (num) => {
   return num;
 }
+//create empty array named mostRepetitive;
+//crate empty object which will count number of occurences in array
+//iterate through data and count number of occurences and save it into empty object
+//create empty array which will save the number of occurences in array
+// create the variable named mostRepetive which will be the the most repetive
+ //numbers in array
+ //iterate throught the object
+ // if the current key of Value is mostRepetive push the key into the result array
+// return the result array
 module.exports = calculateMode;
