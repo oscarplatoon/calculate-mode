801360b25ab35078ea7e2be7f2518da6531a3f6e
AlexanderS68 <gruntmedic68@gmail.com>
2021-05-30 02:41:44 -0400

    mode solution

--- python/calculate_mode.py
+++ python/calculate_mode.py
@@ -1,2 +1,14 @@
 def calculate_mode():
-    pass
+    counter = 0
+    num = arr[0]
+      
+    for i in arr:
+        #Count method returns number of elements seen
+        current_value = arr.count(i)
+        #If element occurs is more than current counter, replace w/ index
+        if current_value > counter:
+            counter = current_value
+            #Num used to reference element location with most occurence index
+            num = i
+  
+    return num
