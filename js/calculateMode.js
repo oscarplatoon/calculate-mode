dbcaa29951a82755cf0621042eb7867ca80bd4ac
jung341416 <jung341416@gmail.com>
2021-05-30 05:41:40 -0400

    calculateMode Js

--- js/calculateMode.js
+++ js/calculateMode.js
@@ -1,13 +1,33 @@
-const calculateMode = (num) => {
-  return num;
+const calculateMode = (data) => {
+  //create empty array named mostRepetitive;
+  let mostRepetitiveResult = [];
+  //crate empty object which will count number of occurences in array
+  let countObj = {};
+  let dataType = typeof data[0];
+  //iterate through data and count number of occurences and save it into empty object
+  for (let i = 0; i < data.length; i++) {
+    let currentData = data[i];
+    if(countObj[currentData]) {
+      countObj[currentData]++;
+    } else {
+      countObj[currentData] = 1;
+    }
+  }
+  //create empty array which will save the number of occurences in array
+  // create the variable named mostRepetive which will be the the most repetive numbers in array
+  //iterate throught the object
+  let mostRepetitiveNum = Object.values(countObj).sort().pop();
+  for (let key in countObj) {
+    if(countObj[key] === mostRepetitiveNum) {
+  // if the current key of Value is mostRepetive push the key into the result array
+    if(dataType === "number"){
+     mostRepetitiveResult.push(Number(key));
+    } else {
+      mostRepetitiveResult.push(key);
+    }
+    }
+  }
+  // return the result array
+return mostRepetitiveResult;
 }
-//create empty array named mostRepetitive;
-//crate empty object which will count number of occurences in array
-//iterate through data and count number of occurences and save it into empty object
-//create empty array which will save the number of occurences in array
-// create the variable named mostRepetive which will be the the most repetive
- //numbers in array
- //iterate throught the object
- // if the current key of Value is mostRepetive push the key into the result array
-// return the result array
 module.exports = calculateMode;
--- js/calculateMode.test.js
+++ js/calculateMode.test.js
@@ -1,5 +1,10 @@
 // Write your own unit tests here!
 const calculateMode = require('./calculateMode');
 test('will return most frequent value in array', () => {
-  expect(calculateMode(2)).toBe(2);
+  expect(calculateMode([1,2,3,3])).toEqual([3]);
+  expect(calculateMode([4.5,0,0])).toEqual([0]);
+  expect(calculateMode([1.5,-1,1,1.5])).toEqual([1.5]);
+  expect(calculateMode([1,1,2,2])).toEqual([1,2]);
+  expect(calculateMode([1,2,3])).toEqual([1,2,3]);
+  expect(calculateMode(["who", "what", "where", "who"])).toEqual(["who"]);
 })
