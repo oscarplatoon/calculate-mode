f02c1be3611ea9715ff65885fd90de9dfc78500e
paulikas78 <70077913+paulikas78@users.noreply.github.com>
2021-05-29 20:20:30 -0500

    Jonas' calculateMode test cases

--- js/calculateModeSpec.js
+++ js/calculateModeSpec.js
@@ -1,1 +1,20 @@
+const calculateMode = require('./calculateMode');
+
+test("test that mode of [1, 2, 3, 1, 3, 2, 3] === [3]", 
+  () => {
+    expect(calculateMode([1, 2, 3, 1, 3, 2, 3])).toEqual(["3"])
+  }
+)
+
+test("test that mode of [1, 2, 3] === [1, 2, 3]", 
+  () => {
+    expect(calculateMode([1, 2, 3])).toEqual(["1", "2", "3"])
+  }
+)
+
+test("test that mode of ['apples', 'oranges', 'apples', 'pears', 'oranges'] === ['apples', 'oranges']", 
+  () => {
+    expect(calculateMode(['apples', 'oranges', 'apples', 'pears', 'oranges'])).toEqual(['apples', 'oranges'])
+  }
+)
 // Write your own unit tests here!
