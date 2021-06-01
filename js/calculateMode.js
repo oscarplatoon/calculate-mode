50299e1ad05dd2ff53ea2698c0045e1892ab601c
Shawn Kiernan <kiernan.shawn@gmail.com>
2021-06-01 08:19:22 -0500

    Shawn JS mode solution with unittest

--- js/calculateMode.js
+++ js/calculateMode.js
@@ -3,12 +3,10 @@
 //  NUMOCCURENCES has objects with two elements for each member (ELEM, NUMOCCURS)
 //  Count number of occurences of each element in MODEARRAY
 //  Return an array composed of ELEMs with NUMOCCURS in MODEARRAY
-const calculateMode = (modeArray) => {
+exports.calculateMode = (modeArray) => {
   let numOccurences = {};
   
   modeArray.sort();
-
-  console.log(modeArray)
   
   let prev;
 
@@ -21,8 +19,6 @@ const calculateMode = (modeArray) => {
     prev = elem;
   }
 
-  console.log(numOccurences);
-
   let max = 0;
   
   for (let elem in numOccurences) {
@@ -31,17 +27,17 @@ const calculateMode = (modeArray) => {
     }
   }
 
-  console.log(max);
-
   let maxElements = [];
   for (let elem in numOccurences) {
     if (numOccurences[elem] === max) {
+      if (typeof(modeArray[0]) === 'number') {
+        elem = parseInt(elem);
+      }
       maxElements.push(elem)
     }
   }
-
-  console.log(maxElements);
+  console.log(maxElements)
   return maxElements;
 }
 
-calculateMode(["who", "what", "what", "where", "who"]);
+// calculateMode(["who", "what", "what", "where", "who"]);
