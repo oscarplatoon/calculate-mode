8012e362e4f334f06d8e6f05fc063a86e475e4e3
jakewhite5 <jakewhite5@yahoo.com>
2021-05-31 23:00:27 -0700

    python and js working with unittesting

--- python/calculate_mode.py
+++ python/calculate_mode.py
@@ -1,2 +1,24 @@
-def calculate_mode():
-    pass
+# import statistics
+
+# def calculate_mode(array):
+#     (statistics.mode(array))
+
+# calculate_mode(['arf','arf','bark'])
+
+def calculate_mode(array):
+    instance_dict = {}
+    return_list = []
+    for item in array:
+        if item in instance_dict.keys():
+            instance_dict[item] = (instance_dict[item] + 1)
+        else:
+            instance_dict[item] = 1
+    mode_dict = (dict(sorted(instance_dict.items(), key=lambda x: x[1], reverse=True)))
+
+    mode = list(mode_dict.values())[0]
+    for key in mode_dict.items():
+        if key[1] == mode:
+            return_list.append(key[0])
+    return return_list
+
+calculate_mode([1,2,2,3,3])
--- python/calculate_mode_spec.py
+++ python/calculate_mode_spec.py
@@ -1,1 +1,20 @@
 # Write your own unit tests here!
+from calculate_mode import calculate_mode
+import unittest
+
+
+class TestMode(unittest.TestCase):
+
+    def test_for_mode(self):
+        self.assertEqual(calculate_mode(['arf','arf','bark']), ['arf'])
+
+    def test_for_multi_mode(self):
+        self.assertEqual(calculate_mode([1,2,3]), [1,2,3])
+    
+    def test_for_multi_mode(self):
+        self.assertEqual(calculate_mode([1,2,2,2,3,3,3]), [2,3])
+
+
+
+if __name__ == '__main__':
+    unittest.main()
