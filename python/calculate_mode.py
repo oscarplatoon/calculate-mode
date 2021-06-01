dd582a2a73f147dc02a5cae14cb88358d617517f
Marc Stanley <marcson909@gmail.com>
2021-05-31 23:28:28 -0500

    completed python via range for loop and list count method. unit tests all passed

--- python/calculate_mode.py
+++ python/calculate_mode.py
@@ -1,2 +1,24 @@
-def calculate_mode():
-    pass
+def calculate_mode(array):
+    #initialize dict, output list and default count variable
+    mode_dict = {}
+    output = []
+    largest_count = 1
+
+    #for range loop using count method to determine dict key values
+    # if key count is greater than largest count
+    # assign key count to leagest count
+    for n in range(len(array)):
+        mode_dict[array[n]] = array.count(array[n])
+        if array.count(array[n]) > largest_count:
+            largest_count = array.count(array[n])
+    #edge case if input array highest count is 1
+    # else append largest count keys to output list
+    if largest_count == 1:
+        return array
+    else:
+        for k in mode_dict:
+            if mode_dict[k] == largest_count:
+                output.append(k)
+        return output
+
+
